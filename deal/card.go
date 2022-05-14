b4e8c39c52499f4cc973ec1763503a87a243e622
Sergey Lytkin <smlytkin@gmail.com>
2022-05-14 11:39:28 +0300

    Constants from card.go went to globals.go

--- deal/card.go
+++ deal/card.go
@@ -1,66 +1,6 @@
 package deal
 
-const (
-	NumberOfSuits = 4
-	SuitLength    = 8
-	NumberOfHands = 3
-)
-
-var SuitSymbols = [NumberOfSuits]rune{'\u2660', '\u2663', '\u2666', '\u2665'}
-var TrumpSuitSymbols = [NumberOfSuits]rune{'\u2664', '\u2667', '\u2662', '\u2661'}
-var RankSymbols = [SuitLength]rune{'7', '8', '9', 'T', 'J', 'Q', 'K', 'A'}
-
-var SuitSymbolsMap = map[rune]Suit{
-	'\u2660': Spades,
-	'\u2663': Clubs,
-	'\u2666': Diamonds,
-	'\u2665': Hearts,
-}
-
-var RankSymbolsMap = map[rune]Suit{
-	'7': 0,
-	'8': 1,
-	'9': 2,
-	'T': 3,
-	'J': 4,
-	'Q': 5,
-	'K': 6,
-	'A': 7,
-}
-
-type Suit int8
-type Rank int8
-type HandIndex int8
 type CardCode int8
-
-const (
-	Spades Suit = iota
-	Clubs
-	Diamonds
-	Hearts
-	NT
-	InvalidSuit = -1
-)
-
-const (
-	Seven Rank = iota
-	Eight
-	Nine
-	Ten
-	Jack
-	Queen
-	King
-	Ace
-	InvalidRank = -1
-)
-
-const (
-	FirstHand HandIndex = iota
-	SecondHand
-	ThirdHand
-	FourthHand
-	InvalidHand = -1
-)
 
 const InvalidCardCode CardCode = -1
 
@@ -69,6 +9,8 @@ type Card struct {
 	code  CardCode
 	trump bool
 }
+
+var InvalidCard = Card{InvalidCardCode, false}
 
 func NewCard(suit Suit, rank Rank, trump bool) Card {
 	return Card{CardCode(rank) + SuitLength*CardCode(suit), trump}
