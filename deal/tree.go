c326e7db4f1f751ec761ac9d0d305956d0808479
Sergey Lytkin <smlytkin@gmail.com>
2022-05-14 11:12:26 +0300

    Remove interface NodeInfo

--- deal/tree.go
+++ deal/tree.go
@@ -1,25 +1,40 @@
 package deal
 
-type NodeInfo interface {
-	Update(other NodeInfo)
-	TerminalValue(dp *DealPlay) NodeInfo
-}
-
-func ResetNodeInfo(node NodeInfo) NodeInfo {
-	return TrickDistributionCounter{make(map[[NumberOfHands]int8]int)}
-}
+import (
+	"fmt"
+	"sync"
+)
 
 type TrickDistributionCounter struct {
 	trickMap map[[NumberOfHands]int8]int
 }
 
-func (td TrickDistributionCounter) Update(other NodeInfo) {
-	for tricks, nPaths := range other.(TrickDistributionCounter).trickMap {
-		td.trickMap[tricks] += nPaths
-	}
+type NodeCounter int64
+
+type NodeInfo struct {
+	type_        MiniMaxType
+	totalPaths   int64
+	miniMaxPaths int64
+	value        MiniMaxValue
+	bestMove     Card
 }
 
-func (td TrickDistributionCounter) TerminalValue(dp *DealPlay) NodeInfo {
+func ResetNodeInfo(node NodeInfo, dp *DealPlay) NodeInfo {
+	value := MiniMaxInf
+	if dp.MoverType() == MaxType {
+		value = -value
+	}
+	return NodeInfo{dp.MoverType(), 0, 0, value, InvalidCard}
+}
+
+func (td TrickDistributionCounter) Update(other TrickDistributionCounter) TrickDistributionCounter {
+	for tricks, nPaths := range other.trickMap {
+		td.trickMap[tricks] += nPaths
+	}
+	return TrickDistributionCounter{td.trickMap}
+}
+
+func (td TrickDistributionCounter) TerminalValue(dp *DealPlay) TrickDistributionCounter {
 	result := make(map[[NumberOfHands]int8]int)
 	result[dp.result]++
 	return TrickDistributionCounter{result}
@@ -35,34 +50,104 @@ type Tree struct {
 	play   *DealPlay
 }
 
-func NewTree(play *DealPlay) Tree {
+func NewTree(play *DealPlay, node NodeInfo) Tree {
 	deckSize := play.deckSize
 	levels := make([]NodeMove, deckSize)
 	var i int8
 	for i = 0; i < deckSize; i++ {
 		levels[i].moves = make([]Card, 0, 1+(deckSize-1-i)/NumberOfHands)
 	}
-	levels[0].NodeInfo = ResetNodeInfo(levels[0].NodeInfo)
+	levels[0].NodeInfo = ResetNodeInfo(node, play)
 	return Tree{levels, play}
+}
+
+func MultiEval(play *DealPlay, policy DensePolicy) {
+	var wg sync.WaitGroup
+	for _, move := range play.GetMoves(policy) {
+		var newMatrix DealMatrix = *play.matrix
+		newPlay := NewDealPlay(&newMatrix, play.trump, play.mover, play.goals)
+		newPlay.DoMove(move)
+		value := MiniMaxInf
+		if newPlay.goals[newPlay.mover] == MaxType {
+			value = -value
+		}
+		tree := NewTree(newPlay, NodeInfo{newPlay.goals[newPlay.mover], 0, 0, value, InvalidCard})
+		wg.Add(1)
+		// fmt.Printf("Move: %v, trick: %v, deck size: %d\n", move, newPlay.lastTrick(), newPlay.deckSize)
+		go func() {
+			defer wg.Done()
+			tree.Eval(policy, 0)
+			fmt.Println("Node type:", tree.levels[0].NodeInfo.type_)
+			fmt.Println("Total paths:", tree.levels[0].NodeInfo.totalPaths)
+			fmt.Println("MiniMax paths:", tree.levels[0].NodeInfo.miniMaxPaths)
+			fmt.Println("Node value:", tree.levels[0].NodeInfo.value)
+			fmt.Println("Best move:", tree.levels[0].NodeInfo.bestMove)
+		}()
+	}
+	wg.Wait()
+}
+
+func (t Tree) TerminalValue() NodeInfo {
+	var result MiniMaxValue
+	dp := t.play
+	if (dp.goals[0] == MaxType && dp.goals[1] == MinType && dp.goals[2] == MinType) ||
+		(dp.goals[0] == MinType && dp.goals[1] == MaxType && dp.goals[2] == MaxType) {
+		result = MiniMaxValue(dp.result[0])
+	} else if (dp.goals[0] == MinType && dp.goals[1] == MaxType && dp.goals[2] == MinType) ||
+		(dp.goals[0] == MaxType && dp.goals[1] == MinType && dp.goals[2] == MaxType) {
+		result = MiniMaxValue(dp.result[1])
+	} else if (dp.goals[0] == MinType && dp.goals[1] == MinType && dp.goals[2] == MaxType) ||
+		(dp.goals[0] == MaxType && dp.goals[1] == MaxType && dp.goals[2] == MinType) {
+		result = MiniMaxValue(dp.result[2])
+	}
+	return NodeInfo{UnknownType, 1, 1, result, InvalidCard}
+}
+
+func (t Tree) Update(level int8) NodeInfo {
+	current := t.levels[level].NodeInfo
+	var next NodeInfo
+	if t.play.deckSize > 0 {
+		next = t.levels[level+1].NodeInfo
+	} else {
+		next = t.TerminalValue()
+	}
+	if current.type_ == MinType {
+		if next.value < current.value {
+			current.miniMaxPaths = next.miniMaxPaths
+			current.value = next.value
+			current.bestMove = t.play.LastMove()
+		} else if next.value == current.value {
+			current.miniMaxPaths += next.miniMaxPaths
+		}
+	} else if current.type_ == MaxType {
+		if next.value > current.value {
+			current.miniMaxPaths = next.miniMaxPaths
+			current.value = next.value
+			current.bestMove = t.play.LastMove()
+		} else if next.value == current.value {
+			current.miniMaxPaths += next.miniMaxPaths
+		}
+	}
+	current.totalPaths += next.totalPaths
+	return current
 }
 
 func (t Tree) Eval(policy DensePolicy, level int8) {
 	if t.play.deckSize == 0 {
 		return
 	}
-	if len(t.levels[level].moves) == 0 {
-		t.levels[level].moves = t.play.GetMoves(policy)
-	}
+	t.levels[level].moves = t.play.GetMoves(policy)
 	for _, move := range t.levels[level].moves {
+		// fmt.Println("Doing move", move)
 		t.play.DoMove(move)
+		// fmt.Println("Current trick:", t.play.lastTrick())
 		if t.play.deckSize > 0 {
-			t.levels[level+1].NodeInfo = ResetNodeInfo(t.levels[level+1].NodeInfo)
+			t.levels[level+1].NodeInfo = ResetNodeInfo(t.levels[level].NodeInfo, t.play)
+			// fmt.Printf("Level %d reset to %v\n", level+1, t.levels[level+1].NodeInfo)
 			t.Eval(policy, level+1)
-			t.levels[level].NodeInfo.Update(t.levels[level+1].NodeInfo)
-		} else {
-			t.levels[level].Update(t.levels[level].TerminalValue(t.play))
 		}
+		t.levels[level].NodeInfo = t.Update(level)
+		// fmt.Println("Undoing move", move)
 		t.play.UndoMove()
 	}
-	t.levels[level].moves = t.levels[level].moves[:0]
 }
--- deal/tree_test.go
+++ deal/tree_test.go
@@ -3,62 +3,170 @@ package deal
 import (
 	"fmt"
 	"log"
-	"time"
 )
 
-func evalDealFromStrings(handStrings []string, delimiter rune, trump Suit, firstMover HandIndex) {
+func evalDealFromStrings(handStrings []string, delimiter rune,
+	trump Suit, firstMover HandIndex, goals DealGoals) Tree {
 	dealMatrix, err := DealMatrixFromStrings(handStrings, delimiter)
 	if err != nil {
 		log.Fatal(err)
 	}
-	dp := NewDealPlay(&dealMatrix, trump, firstMover)
-	root := NewTree(dp)
-	root.Eval(Min, 0)
-	for k, v := range root.levels[0].NodeInfo.(TrickDistributionCounter).trickMap {
-		fmt.Println(k, v)
+	dp := NewDealPlay(&dealMatrix, trump, firstMover, goals)
+	// root := NewTree(dp, NodeCounter(0))
+	//root := NewTree(dp, TrickDistributionCounter{})
+	value := MiniMaxInf
+	if goals[firstMover] == MaxType {
+		value = -value
 	}
+	root := NewTree(dp, NodeInfo{goals[firstMover], 0, 0, value, InvalidCard})
+	root.Eval(Max, 0)
+	fmt.Println("Node type:", root.levels[0].NodeInfo.type_)
+	fmt.Println("Total paths:", root.levels[0].NodeInfo.totalPaths)
+	fmt.Println("MiniMax paths:", root.levels[0].NodeInfo.miniMaxPaths)
+	fmt.Println("Node value:", root.levels[0].NodeInfo.value)
+	fmt.Println("Best move:", root.levels[0].NodeInfo.bestMove)
+	return root
+	/*
+		for k, v := range root.levels[0].NodeInfo.(TrickDistributionCounter).trickMap {
+			fmt.Println(k, v)
+		}*/
 }
 
 func ExampleEval_1() {
 	handStrings := []string{"K", "Q", "A"}
-	evalDealFromStrings(handStrings, '.', Clubs, SecondHand)
+	fmt.Println("First hand plays misere, second hand to move:")
+	evalDealFromStrings(handStrings, '.', NT, SecondHand, FirstHandMisere)
 	handStrings = []string{"K", "A", "Q"}
-	evalDealFromStrings(handStrings, '.', NT, FirstHand)
+	fmt.Println("Second hand plays contract, first hand to move:")
+	evalDealFromStrings(handStrings, '.', Spades, FirstHand, SecondHandContract)
 	handStrings = []string{"K", "8", "J"}
-	evalDealFromStrings(handStrings, '.', Spades, ThirdHand)
+	fmt.Println("Third hand plays misere, third hand to move:")
+	evalDealFromStrings(handStrings, '.', NT, ThirdHand, ThirdHandMisere)
 	// Output:
-	// [0 0 1] 1
-	// [0 1 0] 1
-	// [1 0 0] 1
+	// First hand plays misere, second hand to move:
+	// Node type: 1
+	// Total paths: 1
+	// MiniMax paths: 1
+	// Node value: 0
+	// Best move: ♠Q
+	// Second hand plays contract, first hand to move:
+	// Node type: 0
+	// Total paths: 1
+	// MiniMax paths: 1
+	// Node value: 1
+	// Best move: ♤K
+	// Third hand plays misere, third hand to move:
+	// Node type: 0
+	// Total paths: 1
+	// MiniMax paths: 1
+	// Node value: 0
+	// Best move: ♠J
 }
 
 func ExampleEval_2() {
 	handStrings := []string{"K.9", "AQ", "T.Q"}
-	fmt.Println("First hand move:")
-	evalDealFromStrings(handStrings, '.', NT, FirstHand)
-	fmt.Println("Second hand move:")
-	evalDealFromStrings(handStrings, '.', NT, SecondHand)
-	fmt.Println("Third hand move:")
-	evalDealFromStrings(handStrings, '.', NT, ThirdHand)
+	fmt.Println("Third hand plays contract, first hand to move:")
+	evalDealFromStrings(handStrings, '.', NT, FirstHand, ThirdHandContract)
+	fmt.Println("Second hand plays contract, second hand to move:")
+	evalDealFromStrings(handStrings, '.', NT, SecondHand, SecondHandContract)
+	fmt.Println("First hand plays misere, third hand to move:")
+	evalDealFromStrings(handStrings, '.', NT, ThirdHand, FirstHandMisere)
 	// Output:
-	// First hand move:
-	// [0 2 0] 1
-	// [1 0 1] 2
-	// [0 1 1] 1
-	// Second hand move:
-	// [1 0 1] 1
-	// [0 2 0] 1
-	// Third hand move:
-	// [1 0 1] 2
-	// [0 2 0] 1
-	// [0 1 1] 1
+	// Third hand plays contract, first hand to move:
+	// Node type: 0
+	// Total paths: 4
+	// MiniMax paths: 1
+	// Node value: 0
+	// Best move: ♠K
+	// Second hand plays contract, second hand to move:
+	// Node type: 1
+	// Total paths: 2
+	// MiniMax paths: 1
+	// Node value: 2
+	// Best move: ♠A
+	// First hand plays misere, third hand to move:
+	// Node type: 1
+	// Total paths: 4
+	// MiniMax paths: 2
+	// Node value: 1
+	// Best move: ♠T
 }
 
-func ExampleEval_time() {
-	begin := time.Now()
-	handStrings := []string{"♠A9♦QJ♥J7", "♠QJ♣K8♦9♥10", "♣AQ♦A1087"}
-	evalDealFromStrings(handStrings, NullDelimiter, Diamonds, ThirdHand)
-	fmt.Println(time.Since(begin))
+func ExampleEval_3() {
+	handStrings := []string{"KJ", "Q9", "A8"}
+	fmt.Println("First hand plays contract, first hand to move:")
+	evalDealFromStrings(handStrings, '.', NT, FirstHand, FirstHandContract)
+	fmt.Println("First hand plays contract, second hand to move:")
+	evalDealFromStrings(handStrings, '.', NT, SecondHand, FirstHandContract)
+	fmt.Println("First hand plays contract, third hand to move:")
+	evalDealFromStrings(handStrings, '.', NT, ThirdHand, FirstHandContract)
 	// Output:
-	// long time...
+	// First hand plays contract, first hand to move:
+	// Node type: 1
+	// Total paths: 8
+	// MiniMax paths: 2
+	// Node value: 0
+	// Best move: ♠J
+	// First hand plays contract, second hand to move:
+	// Node type: 0
+	// Total paths: 8
+	// MiniMax paths: 6
+	// Node value: 1
+	// Best move: ♠9
+	// First hand plays contract, third hand to move:
+	// Node type: 0
+	// Total paths: 8
+	// MiniMax paths: 4
+	// Node value: 1
+	// Best move: ♠8
+}
+
+func ExampleEval_4() {
+	handStrings := []string{"KJ.Q8.AQ", "Q9.AKJ.J", "AT.9.K98"}
+	fmt.Println("First hand plays contract, first hand to move:")
+	evalDealFromStrings(handStrings, '.', Diamonds, FirstHand, ThirdHandContract)
+	// Output:
+	// First hand plays contract, first hand to move:
+	// Node type: 0
+	// Total paths: 37288
+	// MiniMax paths: 8598
+	// Node value: 2
+	// Best move: ♠J
+}
+
+func ExampleEval_5() {
+	handStrings := []string{"♠109♣7♦QJT♥A", "♠QJ♣QJ109♦9", "♠AK♣A♦AK87"}
+	// fmt.Println("First hand:", evalDealFromStrings(handStrings, NullDelimiter, Diamonds, FirstHand))
+	// fmt.Println("Second hand:", evalDealFromStrings(handStrings, NullDelimiter, Diamonds, SecondHand))
+	evalDealFromStrings(handStrings, NullDelimiter, Diamonds, FirstHand, ThirdHandContract)
+	evalDealFromStrings(handStrings, NullDelimiter, Diamonds, SecondHand, ThirdHandContract)
+	evalDealFromStrings(handStrings, NullDelimiter, Diamonds, ThirdHand, ThirdHandContract)
+	// Output:
+	// Node type: 0
+	// Total paths: 5202
+	// MiniMax paths: 885
+	// Node value: 5
+	// Best move: ♥A
+	// Node type: 0
+	// Total paths: 4317
+	// MiniMax paths: 2061
+	// Node value: 6
+	// Best move: ♠Q
+	// Node type: 1
+	// Total paths: 4317
+	// MiniMax paths: 2061
+	// Node value: 6
+	// Best move: ♠A
+}
+
+func ExampleMultiEval() {
+	handStrings := []string{"♠109♣7♦QJT♥A", "♠QJ♣QJ109♦9", "♠AK♣A♦AK87"}
+	dealMatrix, err := DealMatrixFromStrings(handStrings, NullDelimiter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dp := NewDealPlay(&dealMatrix, Diamonds, FirstHand, ThirdHandContract)
+	MultiEval(dp, Max)
+	// Output:
+	// Node type: 0
 }
