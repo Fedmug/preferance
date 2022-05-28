00d19595484e15e41afc4054dc3e6d918bb766af
Sergey Lytkin <smlytkin@gmail.com>
2022-05-28 10:19:47 +0300

    Add combinations*

---
+++ deal/combinations.go
@@ -0,0 +1,293 @@
+package deal
+
+import (
+	"log"
+)
+
+const (
+	byteLen = 8
+	byteCap = 256
+)
+
+type Endian int8
+
+const (
+	little Endian = iota
+	big
+)
+
+var BinomialCoefficients [byteLen + 1][byteLen + 1]int
+
+// Given first and second hand codes, retrieve squeezed second hand code, e. g.
+// 00110100, 01000010 -> 00001010 if endian = little
+// 00110100, 01000010 -> 01010000 if endian = big
+var handsMapLittle [byteCap][byteCap]SuitHandCode
+var handsMapBig [byteCap][byteCap]SuitHandCode
+
+// Given first and second hand codes, retrieve unsqueezed second hand code, e. g.
+// 00010100, 00001101 -> 00101001 if endian = little
+// 00010100, 00110100 -> 00101001 if endian = big
+var inverseHandsMapLittle [byteCap][byteCap]SuitHandCode
+var inverseHandsMapBig [byteCap][byteCap]SuitHandCode
+
+type ChaseSequences struct {
+	sequence        [byteLen + 1][byteLen + 1][]SuitHandCode
+	sequenceToIndex [byteLen + 1][byteLen + 1][byteCap]int
+}
+
+var chase ChaseSequences
+
+func squeezeSecondHandCode(firstHandCode, secondHandCode SuitHandCode, endian Endian) SuitHandCode {
+	var i, result, shift SuitHandCode
+	for i = 0; i < byteLen; i++ {
+		index := i
+		if endian == big {
+			index = byteLen - 1 - i
+		}
+		if firstHandCode&(1<<index) == 0 {
+			if endian == little {
+				result |= (secondHandCode & (1 << index)) >> shift
+			} else if endian == big {
+				result |= (secondHandCode & (1 << index)) << shift
+			} else {
+				log.Fatalf("unknown endian: %v", endian)
+			}
+		} else {
+			shift++
+		}
+	}
+	return result
+}
+
+func initMaps() {
+	for first := 0; first < byteCap; first++ {
+		for second := 0; second < byteCap; second++ {
+			if first&second == 0 {
+				squeezedCodeLittle := squeezeSecondHandCode(SuitHandCode(first), SuitHandCode(second), little)
+				handsMapLittle[first][second] = squeezedCodeLittle
+				inverseHandsMapLittle[first][squeezedCodeLittle] = SuitHandCode(second)
+
+				squeezedCodeBig := squeezeSecondHandCode(SuitHandCode(first), SuitHandCode(second), big)
+				handsMapBig[first][second] = squeezedCodeBig
+				inverseHandsMapBig[first][squeezedCodeBig] = SuitHandCode(second)
+			}
+		}
+	}
+}
+
+func squeeze(firstHandCode, secondHandCode SuitHandCode, endian Endian) SuitHandCode {
+	if endian == little {
+		return handsMapLittle[firstHandCode][secondHandCode]
+	}
+	return handsMapBig[firstHandCode][secondHandCode]
+}
+
+func unsqueeze(firstHandCode, secondHandCode SuitHandCode, endian Endian) SuitHandCode {
+	if endian == little {
+		return inverseHandsMapLittle[firstHandCode][secondHandCode]
+	}
+	return inverseHandsMapBig[firstHandCode][secondHandCode]
+}
+
+func initBinomials() {
+	for i := 0; i <= byteLen; i++ {
+		BinomialCoefficients[i][0] = 1
+		BinomialCoefficients[i][i] = 1
+		for j := 1; j < i; j++ {
+			BinomialCoefficients[i][j] = BinomialCoefficients[i-1][j] + BinomialCoefficients[i-1][j-1]
+		}
+	}
+}
+
+func initChase() {
+	for i := 0; i <= byteLen; i++ {
+		for j := 0; j <= byteLen; j++ {
+			for k := 0; k < byteCap; k++ {
+				chase.sequenceToIndex[i][j][k] = -1
+			}
+		}
+	}
+	for n := 0; n <= byteLen; n++ {
+		for s := 0; s <= n; s++ {
+			chase.sequence[n][s] = make([]SuitHandCode, 0, BinomialCoefficients[n][s])
+			var code SuitHandCode
+			for i := 0; i < n-s; i++ {
+				code += 1 << (s + i)
+			}
+			w := make([]uint8, n+1)
+			for i := 0; i <= n; i++ {
+				w[i] = 1
+			}
+			r := s
+			// begin, end := -1, -1
+			if r == 0 {
+				r = n - s
+			}
+			for i := 0; i < BinomialCoefficients[n][s]; i++ {
+				chase.sequence[n][s] = append(chase.sequence[n][s], code)
+				chase.sequenceToIndex[n][s][code] = i
+				j := r
+				for w[j] == 0 {
+					w[j] = 1
+					j++
+				}
+				if j == n {
+					break
+				}
+				w[j] = 0
+				if code&(1<<j) > 0 {
+					if j%2 == 1 || code&(1<<(j-2)) > 0 {
+						code -= 1 << j
+						code += 1 << (j - 1)
+						// begin, end = n-1-j, n-j
+						if r == j && j > 1 {
+							r = j - 1
+						} else if r == j-1 {
+							r = j
+						}
+					} else {
+						code -= 1 << j
+						code += 1 << (j - 2)
+						// begin, end = n-1-j, n+1-j
+						if r == j {
+							r = 1
+							if j-2 > 1 {
+								r = j - 2
+							}
+						} else if r == j-2 {
+							r = j - 1
+						}
+					}
+				} else {
+					if j%2 == 0 || code&(1<<(j-1)) > 0 {
+						code += 1 << j
+						code -= 1 << (j - 1)
+						// begin, end = n-1-j, n-j
+						if r == j && j > 1 {
+							r = j - 1
+						} else if r == j-1 {
+							r = j
+						}
+					} else {
+						code += 1 << j
+						code -= 1 << (j - 2)
+						// begin, end = n-1-j, n+1-j
+						if r == j-2 {
+							r = j
+						} else if r == j-1 {
+							r = j - 2
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func init() {
+	initMaps()
+	initBinomials()
+	initChase()
+}
+
+func suitToIndex(handSizes [NumberOfHands]int8, handCodes [NumberOfHands]SuitHandCode, endian Endian) int {
+	n := handSizes[0] + handSizes[1] + handSizes[2]
+	squeezedSecondHandCode := squeeze(handCodes[0], handCodes[1], endian)
+	firstIndex := chase.sequenceToIndex[n][n-handSizes[0]][handCodes[0]]
+	secondIndex := chase.sequenceToIndex[n-handSizes[0]][handSizes[2]][squeezedSecondHandCode]
+	return firstIndex*BinomialCoefficients[n-handSizes[0]][handSizes[1]] + secondIndex
+}
+
+func indexToSuitCodes(handSizes [NumberOfHands]int8, index int, endian Endian) [NumberOfHands]SuitHandCode {
+	n := handSizes[0] + handSizes[1] + handSizes[2]
+	firstIndex := index / BinomialCoefficients[n-handSizes[0]][handSizes[1]]
+	secondIndex := index % BinomialCoefficients[n-handSizes[0]][handSizes[1]]
+	var result [NumberOfHands]SuitHandCode
+	result[0] = chase.sequence[n][n-handSizes[0]][firstIndex]
+	result[1] = unsqueeze(result[0], chase.sequence[n-handSizes[0]][handSizes[2]][secondIndex], endian)
+	sum := (1 << n) - 1
+	if endian == big {
+		sum <<= 8 - n
+	}
+	result[2] = SuitHandCode(sum) - result[0] - result[1]
+	return result
+}
+
+/*
+func multinomial(ns []uint8) uint64 {
+	//sort.Slice(ns, func(i, j int) bool { return ns[i] < ns[j] })
+	var sum uint8
+	for _, n := range ns {
+		sum += n
+	}
+	var result uint64 = 1
+	i := sum
+	for _, n := range ns[:len(ns)-1] {
+		var j uint8
+		for j = 1; j <= n; j++ {
+			result *= uint64(i)
+			result /= uint64(j)
+			i -= 1
+		}
+	}
+	return result
+}
+
+/*
+func multipermToIndex(multiperm []uint8, ns []uint8) uint64 {
+
+	   Calculates index of a multiset permutation,
+	   see Knuth, Art of Programming, 7.2.1.2, ex. 4
+	   :param ns: sizes of elements of the multiset, ns = (n_1, ..,, n_k)
+	   :param multiperm: permutation of the multiset {n_1*0, ..., n_k*(k-1)}
+	   :return: uint64, index, 0 <= index < (n; ns) (multinomial coefficient)
+
+	var result uint64
+	for _, element := range multiperm {
+		multi_coef := multinomial(ns)
+		var n uint8
+		for _, size := range ns {
+			n += size
+		}
+		var j uint8
+		for ; j < element; j++ {
+			result += multi_coef * uint64(ns[j]) / uint64(n)
+		}
+		ns[element]--
+	}
+	return result
+}
+
+/*
+func indexToMultiperm(index uint64, ns []uint8) []uint8 {
+
+        Calculates multiset permutation by its index,
+        see Knuth, Art of Programming, 7.2.1.2, ex. 4
+        :param ns: sizes of elements of the multiset, ns = (n_1, ..,, n_k)
+        :param index: 0 <= index < (n; ns) (multinomial coefficient)
+        :return: permutation of the multiset {n_1*0, ..., n_k*(k-1)}
+
+	var n uint8
+	var i int = -1
+	for _, size := range ns {
+		n += size
+	}
+    if n == 1 {
+		result := make([]uint8, 1)
+        return np.array(np.argmax(ns))
+	}
+    multi_coef = multinomial(ns)
+    assert index < multi_coef, "Index should be less than multinomial coefficient!"
+    cum_sum = 0
+    for j in range(len(ns)):
+        N_j = multi_coef * ns[j] // ns.sum()
+        if index < cum_sum + N_j:
+            sub_ns = np.copy(ns)
+            sub_ns[j] -= 1
+            if sub_ns[j] == 0:
+                np.delete(sub_ns, j)
+            sub_result = index2multiperm(index - cum_sum, sub_ns)
+            return np.append(j, sub_result)
+        cum_sum += N_j
+}
+*/
---
+++ deal/combinations_test.go
@@ -0,0 +1,207 @@
+package deal
+
+import (
+	"fmt"
+	"math/bits"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func ExampleSqueezeSecondHandCode() {
+	var firstHand, secondHand SuitHandCode = 41, 80
+	fmt.Printf("%08b (first hand)\n", firstHand)
+	fmt.Printf("%08b (second hand)\n", secondHand)
+
+	fmt.Printf("%08b (little)\n", squeeze(firstHand, secondHand, little))
+	fmt.Printf("%08b (big)\n", squeeze(firstHand, secondHand, big))
+	// Output:
+	// 00101001 (first hand)
+	// 01010000 (second hand)
+	// 00001100 (little)
+	// 01100000 (big)
+}
+
+func squeezeSecondHandCodeRandom(n int) {
+	for i := 0; i < n; i++ {
+		first := SuitHandCode(rand.Intn(byteCap))
+		second := SuitHandCode(rand.Intn(byteCap))
+		if first&second == 0 {
+			fmt.Printf("%08b (first hand)\n", first)
+			fmt.Printf("%08b (second hand)\n", second)
+
+			fmt.Printf("%08b (little)\n", squeeze(first, second, little))
+			fmt.Printf("%08b (big)\n\n", squeeze(first, second, big))
+		}
+	}
+}
+
+func ExampleSqueezeSecondHandCode_random() {
+	rand.Seed(125)
+	squeezeSecondHandCodeRandom(30)
+	// Output:
+	// 10010010 (first hand)
+	// 01100101 (second hand)
+	// 00011011 (little)
+	// 11011000 (big)
+	//
+	// 11110111 (first hand)
+	// 00000000 (second hand)
+	// 00000000 (little)
+	// 00000000 (big)
+	//
+	// 10000000 (first hand)
+	// 01000011 (second hand)
+	// 01000011 (little)
+	// 10000110 (big)
+	//
+	// 01011010 (first hand)
+	// 00100001 (second hand)
+	// 00000101 (little)
+	// 01010000 (big)
+}
+
+func ExampleChase() {
+	var n, s = 4, 2
+	for i := 0; i < BinomialCoefficients[n][s]; i++ {
+		fmt.Printf("%04b %d\n", chase.sequence[n][s][i], chase.sequenceToIndex[n][s][chase.sequence[n][s][i]])
+	}
+	n, s = 3, 0
+	for i := 0; i < BinomialCoefficients[n][s]; i++ {
+		fmt.Printf("%04b %d\n", chase.sequence[n][s][i], chase.sequenceToIndex[n][s][chase.sequence[n][s][i]])
+	}
+	n, s = 3, 3
+	for i := 0; i < BinomialCoefficients[n][s]; i++ {
+		fmt.Printf("%04b %d\n", chase.sequence[n][s][i], chase.sequenceToIndex[n][s][chase.sequence[n][s][i]])
+	}
+	// Output:
+	// 1100 0
+	// 1001 1
+	// 1010 2
+	// 0110 3
+	// 0101 4
+	// 0011 5
+	// 0111 0
+	// 0000 0
+}
+
+func TestSecondHandMap(t *testing.T) {
+	total := 0
+	for first := 0; first < byteCap; first++ {
+		for second := 0; second < byteCap; second++ {
+			if first&second == 0 {
+				total++
+				squeezedLittle := squeeze(SuitHandCode(first), SuitHandCode(second), little)
+				squeezedBig := squeeze(SuitHandCode(first), SuitHandCode(second), big)
+				unsqueezedLittle := unsqueeze(SuitHandCode(first), squeezedLittle, little)
+				unsqueezedBig := unsqueeze(SuitHandCode(first), squeezedBig, big)
+				require.EqualValues(t, bits.OnesCount8(uint8(second)), bits.OnesCount8(uint8(squeezedLittle)))
+				require.EqualValues(t, bits.OnesCount8(uint8(second)), bits.OnesCount8(uint8(squeezedBig)))
+				require.EqualValues(t, second, unsqueezedLittle)
+				require.EqualValues(t, second, unsqueezedBig)
+				onesCount := bits.OnesCount8(uint8(first))
+				require.EqualValues(t, squeezedLittle, squeezedBig>>onesCount)
+			}
+		}
+	}
+	require.EqualValues(t, 6561, total)
+}
+
+func TestChase(t *testing.T) {
+	for n := 0; n <= byteLen; n++ {
+		for s := 0; s <= n; s++ {
+			for i := 0; i < BinomialCoefficients[n][s]; i++ {
+				code := chase.sequence[n][s][i]
+				require.EqualValues(t, i, chase.sequenceToIndex[n][s][code])
+				if i > 0 {
+					require.EqualValues(t, 2, bits.OnesCount8(uint8(code^chase.sequence[n][s][i-1])))
+				}
+			}
+		}
+	}
+}
+
+func ExampleSuitToIndex() {
+	handSizes := [NumberOfHands]int8{2, 1, 0}
+	fmt.Println(suitToIndex(handSizes, [NumberOfHands]SuitHandCode{0x3, 0x4, 0}, little),
+		suitToIndex(handSizes, [NumberOfHands]SuitHandCode{0x5, 0x2, 0}, little),
+		suitToIndex(handSizes, [NumberOfHands]SuitHandCode{0x6, 0x1, 0}, little))
+
+	handSizes = [NumberOfHands]int8{0, 1, 4}
+	fmt.Println(suitToIndex(handSizes, [NumberOfHands]SuitHandCode{0, 0x1, 0x1e}, little),
+		suitToIndex(handSizes, [NumberOfHands]SuitHandCode{0, 0x2, 0x1d}, little),
+		suitToIndex(handSizes, [NumberOfHands]SuitHandCode{0, 0x4, 0x1b}, little),
+		suitToIndex(handSizes, [NumberOfHands]SuitHandCode{0, 0x8, 0x17}, little),
+		suitToIndex(handSizes, [NumberOfHands]SuitHandCode{0, 0x10, 0xf}, little))
+	// Output:
+	// 2 1 0
+	// 2 3 1 4 0
+}
+
+func TestSuitIndex(t *testing.T) {
+	var first, second, third int8
+	for first = 0; first <= SuitLength; first++ {
+		for second = 0; second <= SuitLength-first; second++ {
+			for third = 0; third <= SuitLength-first-second; third++ {
+				handSizes := [NumberOfHands]int8{first, second, third}
+				maxIndex := BinomialCoefficients[first+second+third][first] *
+					BinomialCoefficients[second+third][second]
+				for index := 0; index < maxIndex; index++ {
+					suitCodes := indexToSuitCodes(handSizes, index, little)
+					for i := 0; i < NumberOfHands; i++ {
+						require.EqualValues(t, handSizes[i], bits.OnesCount8(uint8(suitCodes[i])),
+							fmt.Sprintf("hand size %v does not match code %v", handSizes[i], suitCodes[i]))
+					}
+					indexFromCodes := suitToIndex(handSizes, suitCodes, little)
+					require.EqualValues(t, index, indexFromCodes,
+						fmt.Sprintf("handSizes = %v, suitCodes = %v", handSizes, suitCodes))
+				}
+			}
+		}
+	}
+}
+
+func BenchmarkIndexToSuitCodes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		first := rand.Intn(SuitLength)
+		second := rand.Intn(SuitLength - first)
+		third := rand.Intn(SuitLength - first - second)
+		index := rand.Intn(BinomialCoefficients[first+second+third][first] *
+			BinomialCoefficients[second+third][second])
+		indexToSuitCodes([NumberOfHands]int8{int8(first), int8(second), int8(third)}, index, little)
+	}
+}
+
+func BenchmarkSuitToIndex(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		handSizes := [NumberOfHands]int8{3, 1, 3}
+		handCodes := [NumberOfHands]SuitHandCode{0x46, 0x80, 0x19}
+		suitToIndex(handSizes, handCodes, little)
+	}
+}
+
+func BenchmarkSqueezer(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for first := 0; first < byteLen; first++ {
+			for second := 0; second < byteLen; second++ {
+				if first&second == 0 {
+					code := squeeze(SuitHandCode(first), SuitHandCode(second), little)
+					unsqueeze(SuitHandCode(first), code, little)
+				}
+			}
+		}
+	}
+}
+
+func BenchmarkChaseSeq(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		first := rand.Intn(SuitLength)
+		second := rand.Intn(SuitLength - first)
+		third := rand.Intn(SuitLength - first - second)
+		index := rand.Intn(BinomialCoefficients[first+second+third][first] *
+			BinomialCoefficients[second+third][second])
+		firstIndex := index / BinomialCoefficients[second+third][second]
+		_ = chase.sequence[first+second+third][second+third][firstIndex]
+	}
+}
